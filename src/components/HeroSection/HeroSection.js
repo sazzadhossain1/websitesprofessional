8db45a4a345412389c6963b82bc13a910a35eb72
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-01-30 19:56:40 +0600

    hero section's banner section done with responsiveness

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./HeroSection.css";
 import heroPhoto from "../../accts/hero_photo/heroPhoto.png";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <div className="heroSection_parent_div">
-      <h1 className="attract_h1">
-        Attract new customer and <br /> Increase Your Chances of Success.
-      </h1>
+      <img className="heroPhoto" src={heroPhoto} alt="" />
+      <div className="attract_div">
+        <h1 className="attract_h1">
+          Attract new customer and <br /> Increase Your Chances of Success.
+        </h1>
+        <h1 className="unlock_h1">
+          Unlock Your Online Potential with <br /> Websites and Powerful
+          Marketing Solutions
+        </h1>
+        <Link to="/contact">
+          <button className="contact_us_btn">Contact Us</button>
+        </Link>
+      </div>
     </div>
   );
 };
