a0740034dd1cee3db6bd48c9b493e0624c2cfa4e
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-01-30 18:17:44 +0600

    add background photo in home page

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,7 +3,13 @@ import "./HeroSection.css";
 import heroPhoto from "../../accts/hero_photo/heroPhoto.png";
 
 const HeroSection = () => {
-  return <div className="heroSection_parent_div"> dfdfdfdfdf</div>;
+  return (
+    <div className="heroSection_parent_div">
+      <h1 className="attract_h1">
+        Attract new customer and <br /> Increase Your Chances of Success.
+      </h1>
+    </div>
+  );
 };
 
 export default HeroSection;
