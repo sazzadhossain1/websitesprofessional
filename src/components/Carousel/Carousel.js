923125dc1ed345fe7c7dc1073ac244db96fb7e5f
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-03-09 02:35:09 +0600

    review carousel done

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,24 +6,55 @@ import {
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Carousel = ({ item }) => {
-  console.log(item);
-  const { description, img, name } = item;
+const Carousel = ({ items }) => {
+  // const { description, img, name } = item;
+  console.log(items);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  //   const convertItemToArray = Object.keys(item);
+  //   console.log(convertItemToArray);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = items.length - 1;
+    } else if (newIndex >= items.length) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+  };
+
+  const currentItem = items[activeIndex];
   return (
-    <div>
+    <div
+      className="carousel_item"
+      //   style={{ transform: `translate(-${activeIndex * 100}%)` }}
+    >
       <div className="carousel_flex_div">
         <div className="carousel_Photo_div">
-          <img className="carousel_PhotoThree" src={img} alt="" />
+          <img className="carousel_PhotoThree" src={currentItem.img} alt="" />
           <div className="carousel_name_div">
-            <FontAwesomeIcon className="arrowIcon" icon={faChevronLeft} />
+            <FontAwesomeIcon
+              onClick={() => {
+                updateIndex(activeIndex - 1);
+              }}
+              className="arrowIcon"
+              icon={faChevronLeft}
+            />
             <div>
-              <p>{name}</p>
+              <p>{currentItem.name}</p>
               <p>UPWORK, New York</p>
             </div>
-            <FontAwesomeIcon className="arrowIcon" icon={faChevronRight} />
+            <FontAwesomeIcon
+              onClick={() => {
+                updateIndex(activeIndex + 1);
+              }}
+              className="arrowIcon"
+              icon={faChevronRight}
+            />
           </div>
         </div>
-        <p className="carousel_text_div">{description}</p>
+        <p className="carousel_text_p">{currentItem.description}</p>
       </div>
     </div>
   );
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,4 @@
 import "./Review.css";
-import reviewBackgroundPhoto from "../../accts/reviewPhoto/reviewBackgroundPhoto.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronLeft,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
 
 import reviewPhotoOne from "../../accts/reviewPhoto/reviewPhotoOne.webp";
 import reviewPhotoTwo from "../../accts/reviewPhoto/reviewPhotoTwo.png";
@@ -20,47 +14,47 @@ const Review = () => {
       name: "Standard Room",
       img: reviewPhotoOne,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
+        "aaaaaaaaLorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
     },
     {
       id: 2,
 
-      name: "Standard Room",
+      name: "Jhonas smith",
       img: reviewPhotoTwo,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
+        "bbbbbbbLorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
     },
     {
       id: 3,
 
-      name: "Standard Room",
+      name: "Jhonkar Mahbub",
       img: reviewPhotoThree,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
+        "ccccccccLorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
     },
     {
       id: 4,
 
-      name: "Standard Room",
+      name: "My Teacher",
       img: reviewPhotoFour,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
+        "dddddddddLorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
     },
     {
       id: 5,
 
-      name: "Standard Room",
+      name: "and me",
       img: reviewPhotoFive,
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
+        "eeeeeeeeeLorem ipsum dolor sit amet consectetur adipisicing elit. Commodi architecto voluptatibus facilis quo aliquid perspiciatis. Laudantium at dolores labore accusantium?",
     },
   ];
   return (
-    <div>
-      <div className="review_parent_div"></div>
-      {items.map((item) => (
-        <Carousel item={item} key={item.id}></Carousel>
-      ))}
+    <div className="reviewCarousel">
+      <div className="review_parent_div inner"></div>
+      <div className="inner">
+        <Carousel items={items}></Carousel>
+      </div>
     </div>
   );
 };
