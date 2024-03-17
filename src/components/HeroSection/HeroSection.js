6392e9013b52cfda5ad00ccef9e68c5549ad23d4
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-03-17 12:29:39 +0600

    add key words to title

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./HeroSection.css";
-import heroPhoto from "../../accts/hero_photo/heroPhoto.png";
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
@@ -9,7 +8,7 @@ const HeroSection = () => {
       {/* <img className="heroPhoto" src={heroPhoto} alt="" /> */}
       <div className="attract_div">
         <h1 className="attract_h1">
-          Attract New Customers and Grow Your Business
+          Attract New Customers and <br /> Grow Your Business
         </h1>
         {/* <h1 className="unlock_h1">
           Unlock Your Online Potential with <br /> Websites and Powerful
