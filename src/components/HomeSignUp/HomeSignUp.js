a94fbc6af8c8508b56fd1bedb34321a2b85ad1ec
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-03-18 17:46:51 +0600

    home signUp page recorrection

--- src/components/HomeSignUp/HomeSignUp.js
+++ src/components/HomeSignUp/HomeSignUp.js
@@ -3,27 +3,32 @@ import "./HomeSignUp.css";
 const HomeSignUp = () => {
   return (
     <div className="homeSignUp_parent_div">
-      <div className="homeSignUp_cart_div">
-        <div className="homeSignUp_inner_div">
-          <h1 className="homeSignUP_In_heading">Sign Up</h1>
-          <div>
+      <div className="homeSignUp_grid_div">
+        <div className="homeSignUp_text_div">
+          <h1 className="homeSignUP_In_heading">Sign Up &gt;</h1>
+        </div>
+        <div className="homeSignUp_cart_div">
+          <div className="homeSignUp_inner_div">
+            <h1 className="homeSignUP_In_heading_in_cart">Sign Up &gt;</h1>
             <div>
-              <input
-                className="homeSignUp_input"
-                type="name"
-                placeholder="Full Name"
-              />
+              <div>
+                <input
+                  className="homeSignUp_input"
+                  type="name"
+                  placeholder="Full Name"
+                />
+              </div>
+              <div>
+                <input
+                  className="homeSignUp_input"
+                  type="email"
+                  placeholder="Email"
+                />
+              </div>
             </div>
-            <div>
-              <input
-                className="homeSignUp_input"
-                type="email"
-                placeholder="Email"
-              />
-            </div>
-          </div>
 
-          <button className="homeSignUp_btn">Submit</button>
+            <button className="homeSignUp_btn">Submit</button>
+          </div>
         </div>
       </div>
     </div>
