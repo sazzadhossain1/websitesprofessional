e5b69eb505a1a85431df589ff2688eb25baf5779
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-05-31 17:18:32 +0600

    load api and show data from api to sitemap component

--- src/components/Sitemap/Sitemap.js
+++ src/components/Sitemap/Sitemap.js
@@ -4,10 +4,26 @@ import { useLoaderData } from "react-router-dom";
 
 const Sitemap = () => {
   const getSiteMapdata = useLoaderData();
-  console.log(getSiteMapdata);
+  console.log(getSiteMapdata.data);
+
+  const rootUrl = "https://admin.websitesprofessional.com";
+
+  const mapDatas = getSiteMapdata.data.data;
   return (
     <div className="sitemap_parent-div">
-      <h1>This is site map</h1>
+      {mapDatas.map((data) => (
+        <div className="siteMap_map_div" key={data.id}>
+          <img className="siteMap_photo" src={rootUrl + data.image} alt="" />
+          <p className="siteMap_title">{data.title}</p>
+          <p className="siteMap_sub_title">{data.sub_title}</p>
+          <p className="siteMap_price">Price: {data.price}</p>
+
+          <p
+            dangerouslySetInnerHTML={{ __html: data.details }}
+            className="siteMap_details"
+          ></p>
+        </div>
+      ))}
     </div>
   );
 };
